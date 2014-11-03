2432377337e94f7c9afa37e3e5ffff88ab5addb6
Aniket Schneider <aschneider@visiblemeasures.com>
2014-11-02 21:39:56 -0500

    Adjust code style

--- app/src/main/java/personal/aschneider/charsheet/AbilityScoreView.java
+++ app/src/main/java/personal/aschneider/charsheet/AbilityScoreView.java
@@ -11,76 +11,76 @@ import android.widget.TextView;
  * Created by aschneider on 10/27/14.
  */
 public class AbilityScoreView extends LinearLayout {
-    //TODO: awkward to store mutable abilityScore object here
-    private AbilityScore abilityScore;
+  //TODO: awkward to store mutable abilityScore object here
+  private AbilityScore abilityScore;
 
-    private TextView abilityNameView;
-    private TextView abilityModNameView;
+  private TextView abilityNameView;
+  private TextView abilityModNameView;
 
-    private TextView abilityScoreView;
-    private TextView abilityModView;
+  private TextView abilityScoreView;
+  private TextView abilityModView;
 
-    public AbilityScoreView(Context context) {
-        super(context);
-        inflateViews(context);
-        // TODO: what should happen here?
+  public AbilityScoreView(Context context) {
+    super(context);
+    inflateViews(context);
+    // TODO: what should happen here?
+  }
+
+  public AbilityScoreView(Context context, AttributeSet attrs, int defStyle) {
+    super(context, attrs, defStyle);
+    inflateViews(context);
+
+    readAttributes(context, attrs);
+  }
+
+  public AbilityScoreView(Context context, AttributeSet attrs) {
+    super(context, attrs);
+    inflateViews(context);
+
+    readAttributes(context, attrs);
+  }
+
+  private void readAttributes(Context context, AttributeSet attrs) {
+    TypedArray a = context.obtainStyledAttributes(attrs, R.styleable.AbilityScoreView);
+    String longName = a.getString(R.styleable.AbilityScoreView_statName);
+    if (longName == null) longName = "REPLACE";
+    Integer score = a.getInteger(R.styleable.AbilityScoreView_score, 1);
+    a.recycle();
+
+    setAbilityScore(new AbilityScore(longName, score));
+  }
+
+  public void setAbilityScore(AbilityScore abilityScore) {
+    this.abilityScore = abilityScore;
+    updateViews();
+  }
+
+  public void setScore(int value) {
+    abilityScore.setValue(value);
+    updateViews();
+  }
+
+  private void inflateViews(Context c) {
+    LayoutInflater inflater = LayoutInflater.from(c);
+    inflater.inflate(R.layout.widget_ability_score, this);
+
+    abilityNameView = (TextView) this.findViewById(R.id.abilityNameView);
+    abilityScoreView = (TextView) this.findViewById(R.id.abilityScoreView);
+    abilityModNameView = (TextView) this.findViewById(R.id.abilityModNameView);
+    abilityModView = (TextView) this.findViewById(R.id.abilityModView);
+  }
+
+  private void updateViews() {
+    abilityNameView.setText(abilityScore.getLongName());
+    abilityScoreView.setText(Integer.valueOf(abilityScore.getValue()).toString());
+    abilityModNameView.setText(abilityScore.getShortName());
+    Integer mod = Integer.valueOf(abilityScore.getModifier());
+    String modString;
+    if (mod >= 0) {
+      modString = "+" + mod.toString();
+    } else {
+      modString = mod.toString();
     }
-
-    public AbilityScoreView(Context context, AttributeSet attrs, int defStyle) {
-        super(context, attrs, defStyle);
-        inflateViews(context);
-
-        readAttributes(context, attrs);
-    }
-
-    public AbilityScoreView(Context context, AttributeSet attrs) {
-        super(context, attrs);
-        inflateViews(context);
-
-        readAttributes(context, attrs);
-    }
-
-    private void readAttributes(Context context, AttributeSet attrs) {
-        TypedArray a = context.obtainStyledAttributes(attrs, R.styleable.AbilityScoreView);
-        String longName = a.getString(R.styleable.AbilityScoreView_statName);
-        if (longName == null) longName = "REPLACE";
-        Integer score = a.getInteger(R.styleable.AbilityScoreView_score, 1);
-        a.recycle();
-
-        setAbilityScore(new AbilityScore(longName, score));
-    }
-
-    public void setAbilityScore(AbilityScore abilityScore) {
-        this.abilityScore = abilityScore;
-        updateViews();
-    }
-
-    public void setScore(int value) {
-        abilityScore.setValue(value);
-        updateViews();
-    }
-
-    private void inflateViews(Context c) {
-        LayoutInflater inflater = LayoutInflater.from(c);
-        inflater.inflate(R.layout.widget_ability_score, this);
-
-        abilityNameView = (TextView) this.findViewById(R.id.abilityNameView);
-        abilityScoreView = (TextView) this.findViewById(R.id.abilityScoreView);
-        abilityModNameView = (TextView) this.findViewById(R.id.abilityModNameView);
-        abilityModView = (TextView) this.findViewById(R.id.abilityModView);
-    }
-
-    private void updateViews() {
-        abilityNameView.setText(abilityScore.getLongName());
-        abilityScoreView.setText(Integer.valueOf(abilityScore.getValue()).toString());
-        abilityModNameView.setText(abilityScore.getShortName());
-        Integer mod = Integer.valueOf(abilityScore.getModifier());
-        String modString;
-        if (mod >= 0) {
-            modString = "+" + mod.toString();
-        } else {
-            modString = mod.toString();
-        }
-        abilityModView.setText(modString);
-    }
+    abilityModView.setText(modString);
+  }
 }
--- app/src/main/java/personal/aschneider/charsheet/Character.java
+++ app/src/main/java/personal/aschneider/charsheet/Character.java
@@ -1,5 +1,98 @@
 package personal.aschneider.charsheet;
 
+import android.content.Context;
+
+import com.google.common.collect.Maps;
+
+import java.util.Map;
+import java.util.UUID;
+
 public class Character {
-	private AbilityScore strength;
+  private Context context;
+
+  private final UUID id;
+  private String name;
+  private int raceId;
+  private int alignmentId;
+  private int level;
+  private int xp;
+  private String description;
+
+  private Map<Integer, Integer> stats;
+
+  public Character(Context c) {
+    context = c;
+
+    id = UUID.randomUUID();
+    name = c.getString(R.string.new_character_name);
+    raceId = R.string.new_character_race;
+    alignmentId = R.string.new_character_alignment;
+    level = 1;
+    xp = 0;
+    description = c.getString(R.string.new_character_desc);
+
+    stats = Maps.newConcurrentMap();
+    stats.put(R.string.ability_strength, 1);
+    stats.put(R.string.ability_dexterity, 1);
+    stats.put(R.string.ability_constitution, 1);
+    stats.put(R.string.ability_intelligence, 1);
+    stats.put(R.string.ability_wisdom, 1);
+    stats.put(R.string.ability_charisma, 1);
+  }
+
+  public UUID getId() {
+    return id;
+  }
+
+  public String getName() {
+    return name;
+  }
+
+  public void setName(String name) {
+    this.name = name;
+  }
+
+  public int getRaceId() {
+    return raceId;
+  }
+
+  public void setRaceId(int race) {
+    this.raceId = race;
+  }
+
+  public int getAlignmentId() {
+    return alignmentId;
+  }
+
+  public void setAlignmentId(int alignment) {
+    this.alignmentId = alignment;
+  }
+
+  public int getLevel() {
+    return level;
+  }
+
+  public void setLevel(int level) {
+    this.level = level;
+  }
+
+  public int getXp() {
+    return xp;
+  }
+
+  public void setXp(int xp) {
+    this.xp = xp;
+  }
+
+  public String getDescription() {
+    return description;
+  }
+
+  public void setDescription(String description) {
+    this.description = description;
+  }
+
+  public int getStatValue(int abilityId) {
+    return stats.get(abilityId);
+  }
 }
